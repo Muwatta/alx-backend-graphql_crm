49d35eaee0d405a89355ebf79f4ab742fc34624c
Muwatta <abdullahmusliudeen@gamil.com>
2025-10-19 09:40:23 +0100

    Fix project schema for ALX GraphQL checks

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,10 +1,14 @@
+# alx_backend_graphql/schema.py
 import graphene
 from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
+# Combine your CRM queries into the project-level Query
 class Query(CRMQuery, graphene.ObjectType):
     pass
 
+# Combine your CRM mutations into the project-level Mutation
 class Mutation(CRMMutation, graphene.ObjectType):
     pass
 
+# Create the main schema that will be exposed at /graphql
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- schema.py
+++
@@ -1,9 +0,0 @@
-# alx_backend_graphql_crm/schema.py
-
-import sys
-import os
-
-# Add the inner project folder to path
-sys.path.append(os.path.join(os.path.dirname(__file__), "alx_backend_graphql_crm"))
-
-from alx_backend_graphql_crm.schema import schema  # import the real schema
