9e48259f94472726c333c8c2d75b09cf00463c57
Muwatta <abdullahmusliudeen@gamil.com>
2025-10-19 11:19:53 +0100

    Fix project schema for ALX_GraphQL checks

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'graphene-django',
-    'django-filters',
+    'graphene_django',
+    'django_filters',
     'crm',
 ]
 
