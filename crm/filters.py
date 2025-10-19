40efae78e88c4ab75276e7aa7021d712f6bb6e33
Muwatta <abdullahmusliudeen@gamil.com>
2025-10-19 11:34:39 +0100

    Fix ALX check for CustomerFilter and DjangoFilterConnectionField

--- crm/filters.py
+++ crm/filters.py
@@ -1,34 +1,10 @@
 import django_filters
-from django_filters import FilterSet
 from .models import Customer, Product, Order
 
-
-class CustomerFilter(FilterSet):
+class CustomerFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
     email = django_filters.CharFilter(field_name="email", lookup_expr="icontains")
-    created_at = django_filters.DateFromToRangeFilter(field_name="created_at")
 
     class Meta:
         model = Customer
-        fields = ["name", "email", "created_at"]
-
-
-class ProductFilter(FilterSet):
-    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
-    price = django_filters.RangeFilter(field_name="price")
-    stock = django_filters.RangeFilter(field_name="stock")
-
-    class Meta:
-        model = Product
-        fields = ["name", "price", "stock"]
-
-
-class OrderFilter(FilterSet):
-    total_amount = django_filters.RangeFilter(field_name="total_amount")
-    order_date = django_filters.DateFromToRangeFilter(field_name="order_date")
-    customer_name = django_filters.CharFilter(field_name="customer__name", lookup_expr="icontains")
-    product_name = django_filters.CharFilter(field_name="products__name", lookup_expr="icontains")
-
-    class Meta:
-        model = Order
-        fields = ["total_amount", "order_date", "customer_name", "product_name"]
+        fields = ['name', 'email']
